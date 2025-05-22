fc43f1fcca481b49ad9153bbb2b5151ba035a642
Navanath Temgire <tnavanath@gmail.com>
2025-05-22 20:36:17 +0530

    Updated gitIgnore

--- features/AddToCart.feature
+++ features/AddToCart.feature
@@ -1,13 +1,18 @@
-Feature: Add To Cart functionality on Sauce Demo
+Feature: SauceDemo E2E Flow
 
-  Background:
-    Given User is on the login page
-    When User logs in with username "standard_user" and password "secret_sauce"
+  Scenario Outline: User logs in, adds an item to cart, and removes it
+    Given the user is on the login page
+    When the user logs in using "<username>" and "<password>"
+    Then the user should be redirected to the inventory page
 
-  Scenario Outline: Add product to cart
-    When User adds "<product>" to the cart
-    Then The cart should contain "<product>"
+    When the user adds "<item>" to the cart
+    And navigates to the cart page
+    Then the cart should display "<item>"
 
-    Examples:
-      | product              |
-      | Sauce Labs Backpack |
+    When the user removes "<item>" from the cart
+    Then the cart should be empty
+
+  Examples:
+    | username      | password     | item                   |
+    | standard_user | secret_sauce | Sauce Labs Backpack    |
+    | standard_user | secret_sauce | Sauce Labs Bike Light  |
--- features/stepDefinations/AddToCart.js
+++ features/stepDefinations/AddToCart.js
@@ -1,22 +1,43 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 
-Given('User is on the login page', async function () {
-  await this.page.goto('https://www.saucedemo.com/');
+let page;
+
+Given('the user is on the login page', async function () {
+  page = await this.browser.newPage();
+  await page.goto('https://www.saucedemo.com');
 });
 
-When('User logs in with username {string} and password {string}', async function (username, password) {
-  const loginPage = this.pom.getLoginPage();
-  await loginPage.login(username, password);
+When('the user logs in using {string} and {string}', async function (username, password) {
+  await page.fill('#user-name', username);
+  await page.fill('#password', password);
+  await page.click('#login-button');
 });
 
-When('User adds {string} to the cart', async function (productName) {
-  const inventoryPage = this.pom.getInventoryPage();
-  await inventoryPage.addItemToCart(productName);
-  await inventoryPage.goToCart();
+Then('the user should be redirected to the inventory page', async function () {
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
-Then('The cart should contain {string}', async function (productName) {
-  const locator = this.page.locator('.inventory_item_name');
-  await expect(locator).toHaveText(productName);
+When('the user adds {string} to the cart', async function (item) {
+  const formattedItemId = item.toLowerCase().replaceAll(' ', '-');
+  await page.click(`#add-to-cart-${formattedItemId}`);
+});
+
+When('navigates to the cart page', async function () {
+  await page.click('.shopping_cart_link');
+});
+
+Then('the cart should display {string}', async function (item) {
+  const cartItem = await page.locator('.inventory_item_name').innerText();
+  expect(cartItem).toBe(item);
+});
+
+When('the user removes {string} from the cart', async function (item) {
+  const formattedItemId = item.toLowerCase().replaceAll(' ', '-');
+  await page.click(`#remove-${formattedItemId}`);
+});
+
+Then('the cart should be empty', async function () {
+  const items = await page.locator('.cart_item').count();
+  expect(items).toBe(0);
 });
